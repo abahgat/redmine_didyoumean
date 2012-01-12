7c98daa0a05dadcbaef5b2d3f93e95eaa12660a2
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-01-13 00:58:44 +0100

    Initial plugin setup

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_didyoumean do
+  name 'Did You Mean plugin'
+  author 'Alessandro Bahgat'
+  description 'A plugin to search for duplicate issues before insertion'
+  version '0.0.1'
+  url 'http://www.github.com/abahgat/didyoumean'
+  author_url 'http://abahgat.com/'
+end
