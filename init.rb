b29fb8f086775c7fed8c5004cb98cb6cdfd0c3e3
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-01-16 19:41:14 +0100

    Updated plugin URL and description

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'redmine'
 Redmine::Plugin.register :redmine_didyoumean do
   name 'Did You Mean plugin'
   author 'Alessandro Bahgat'
-  description 'A plugin to search for duplicate issues before insertion'
+  description 'A plugin to search for duplicate issues before opening them.'
   version '0.0.1'
-  url 'http://www.github.com/abahgat/didyoumean'
+  url 'http://www.github.com/abahgat/redmine_didyoumean'
   author_url 'http://abahgat.com/'
 end
 
