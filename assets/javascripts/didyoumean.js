3dccfcb87d61974f57b7057380a0d1ceb47bbd17
Raibaz <raibaz@gmail.com>
2012-01-17 15:58:07 +0100

    Fixed a typo

--- assets/javascripts/didyoumean.js
+++ assets/javascripts/didyoumean.js
@@ -8,7 +8,7 @@ function observeIssueSubjectField() {
 function drawSimilarIssuesBlock() {
 	var similar_issues_tpl = 
 		'<div class="similar_issues">\
-			<b>Issues with simlar titles</b>\
+			<b>Issues with similar titles</b>\
 			<ul class="issue_list">\
 				<li><a>#1: subject of issue 1</a> (status)</li><li><a>#2: subject of issue 2</a> (status)</li>\
 			</ul>\
