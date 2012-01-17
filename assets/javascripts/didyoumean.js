1f053a9e6190b6a1ccb4810465080050b683dc0e
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-01-17 01:17:16 +0100

    Draft layout for similar issues list.
    Will need to properly display the issue list (current ul margin is too
    large) and fix the div right margin.

--- assets/javascripts/didyoumean.js
+++ assets/javascripts/didyoumean.js
@@ -1,5 +1,17 @@
 function observeIssueSubjectField() {
-  $('issue_subject').observe('keypress', function(event){
+  $('issue_subject').observe('change', function(event){
     console.log(Event.element(event).value);
   });
+  drawSimilarIssuesBlock();
+}
+
+function drawSimilarIssuesBlock() {
+	var similar_issues_tpl = 
+		'<div class="similar_issues">\
+			<b>Issues with simlar titles</b>\
+			<ul class="issue_list">\
+				<li><a>#1: subject of issue 1</a> (status)</li><li><a>#2: subject of issue 2</a> (status)</li>\
+			</ul>\
+		</div>';
+	$('issue_subject').up().insert({after: similar_issues_tpl});
 }
