bc06fc01b202a71b35fc038bb36219f6286571be
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-01-17 23:18:33 +0100

    Added some client-side logic and tidied up code
    * Added some stub JavaScript functions
    * Moved HTML code to partial
    * Similar issues caption is now in the proper _i18n_ files

--- assets/javascripts/didyoumean.js
+++ assets/javascripts/didyoumean.js
@@ -1,17 +1,40 @@
 function observeIssueSubjectField() {
+
   $('issue_subject').observe('change', function(event){
     console.log(Event.element(event).value);
   });
+
   drawSimilarIssuesBlock();
+
 }
 
 function drawSimilarIssuesBlock() {
-	var similar_issues_tpl = 
-		'<div class="similar_issues">\
-			<b>Issues with similar titles</b>\
-			<ul class="issue_list">\
-				<li><a>#1: subject of issue 1</a> (status)</li><li><a>#2: subject of issue 2</a> (status)</li>\
-			</ul>\
-		</div>';
-	$('issue_subject').up().insert({after: similar_issues_tpl});
+  
+  $('issue_subject').up().insert({after: $('similar_issues')});
+
+}
+
+function populateSimilarIssuesBlock(items) {
+  
+  $('issue_list').descendants().remove();
+
+  for (var i = items.length - 1; i >= 0; i--) {
+    var item_html = // display items[i]
+    $('issue_list').insert({top: item_html});
+  };
+
+  if (! $('similar_issues').visible()) {
+    $('similar_issues').show();
+  }
+
+}
+
+function emptySimilarIssuesBlock() {
+  
+  $('issue_list').descendants().remove();
+
+  if ($('similar_issues').visible()) {
+    $('similar_issues').hide();
+  }
+
 }
