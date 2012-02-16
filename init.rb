e06a43938963bc7fcbcac34d888b5e8ab9b01ae6
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-02-16 15:09:04 +0100

    Added license files and preparing manifest for release 1.0.0

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_didyoumean do
-  name 'Did You Mean plugin'
-  author 'Alessandro Bahgat'
+  name 'Did You Mean?'
+  author 'Alessandro Bahgat and Mattia Tommasone'
   description 'A plugin to search for duplicate issues before opening them.'
-  version '0.0.1'
+  version '1.0.0'
   url 'http://www.github.com/abahgat/redmine_didyoumean'
   author_url 'http://abahgat.com/'
   settings(:default => {'show_only_open' => '1', 'project_filter' => '1'}, :partial => 'settings/settings')
