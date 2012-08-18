87f80625600c0bc1ca2cfae3b0f0c9e572efb48b
Vladimir <nettsundere@gmail.com>
2012-08-18 05:19:10 +0400

    Added html symbols filtration.

--- assets/javascripts/didyoumean.js
+++ assets/javascripts/didyoumean.js
@@ -47,7 +47,36 @@ function populateSimilarIssuesBlock(data) {
 }
 
 function displayItem(item) {
-  return '<li><a href="' + dym.issue_url + '/' + item.id + '">' + item.tracker.name + ' #' + item.id + ' &ndash; ' + item.subject + '</a> (' + item.status.name + ' ' + dym.label_in + ' ' + item.project.name +')</li>';
+
+  var issue_url = sanitize(dym.issue_url + '/' + item.id);
+  var tracker_name = sanitize(item.tracker.name);
+  var item_id = sanitize('#' + item.id);
+  var item_subject = sanitize(item.subject);
+  var item_status = sanitize(item.status.name);
+  var project_name = sanitize(item.project.name);
+
+  var item_html = '<li><a href="' + issue_url + '">' 
+    + tracker_name 
+    + ' ' + item_id
+    + ' &ndash; ' 
+    + item_subject
+    + '</a> ('
+    + item_status
+    + ' ' 
+    + dym.label_in
+    + ' ' + project_name
+    + ')</li>';
+
+  return item_html;
+}
+
+function sanitize(value) {
+  
+  var html_safe = value.replace(/[<]+/g, '&lt;')
+                      .replace(/[>]+/g, '&gt;')
+                      .replace(/["]+/g, '&quot;')
+                      .replace(/[']+/g, '&#039;');
+  return html_safe;
 }
 
 function emptySimilarIssuesBlock() {
@@ -57,5 +86,4 @@ function emptySimilarIssuesBlock() {
   if ($('similar_issues').visible()) {
     $('similar_issues').hide();
   }
-
 }
