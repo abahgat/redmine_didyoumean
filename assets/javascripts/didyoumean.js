8887fc355dfca2934695a3d4ab84a00a10bacaf2
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-10-30 00:27:36 +0100

    Updated JavaScript to work with both jQuery and Prototype
    Redmine 2.1 uses jQuery instead of Prototype, so the client-side code
    had to be rewritten to support both earlier and older versions.
    While this change introduces several checks on window.jQuery, it tries
    to duplicate the least possible lines of code.
    Closes #24

--- assets/javascripts/didyoumean.js
+++ assets/javascripts/didyoumean.js
@@ -1,49 +1,80 @@
 function observeIssueSubjectField(project_id, issue_id) {
 
-  $('issue_subject').observe('change', function(event){
+  var drawCallback = function(data) {
+    if (data.total) {
+      drawSimilarIssuesBlock();
+      populateSimilarIssuesBlock(data);
+    }
+  };
+
+  var handleChange = function(event) {
+
     emptySimilarIssuesBlock();
     var url = dym.search_url;
-    new Ajax.Request(url, {
-      parameters: {
-        project_id: project_id,
-        issue_id: issue_id,
-        query: Event.element(event).value
-      },
-      onSuccess: function(transport) {
-        var data = transport.responseJSON;
-        if(data.total) {
-          drawSimilarIssuesBlock();
-          populateSimilarIssuesBlock(data);
-        }
-      },
-      evalJSON: true
-    });  
-  });
+    var parameters = {
+      project_id: project_id,
+      issue_id: issue_id,
+      query: (window.jQuery ? $(this).val() : Event.element(event).value)
+    };
+
+    if (window.jQuery) {
+      new $.ajax(url, {
+        data: parameters,
+        success: function(data, textStatus, jqXHR) {
+          drawCallback(data);
+        }});
+    } else {
+      new Ajax.Request(url, {
+        parameters: parameters,
+        onSuccess: function(transport) {
+          drawCallback(transport.responseJSON);
+        },
+        evalJSON: true
+      });
+    }
+  }
+  
+  if (window.jQuery) {
+    getElem('issue_subject').change(handleChange);
+  } else {
+    getElem('issue_subject').observe('change', handleChange);
+  }
 }
 
 function drawSimilarIssuesBlock() {
-  
-  $('issue_subject').up().insert({after: $('similar_issues')});
+
+  if (window.jQuery) {
+    getElem('issue_subject').parent().after(getElem('similar_issues'));
+  } else {
+    getElem('issue_subject').up().insert({after: getElem('similar_issues')});
+  }
 
 }
 
 function populateSimilarIssuesBlock(data) {
-  
-  $('similar_issues_list').innerHTML = '';
+
+  setText(getElem('similar_issues_list'), '');
   
   var items = data.issues;
   for (var i = items.length - 1; i >= 0; i--) {
     var item_html = displayItem(items[i]);
-    $('similar_issues_list').insert({top: item_html});
+    if (window.jQuery) {
+      getElem('similar_issues_list').prepend(item_html);
+    } else {
+      getElem('similar_issues_list').insert({top: item_html});
+    }
   };
 
- $('issues_count').innerHTML = data.total;
-  if (! $('similar_issues').visible()) {
-    $('similar_issues').show();
-  }
+  setText(getElem('issues_count'), data.total);
+  getElem('similar_issues').show();
+
   if (data.total > data.issues.length) {
     var more = data.total - data.issues.length;
-    $('similar_issues_list').insert({bottom: '<li>+' + more + ' ' + dym.label_more + '</li>'});
+    if (window.jQuery) {
+      getElem('similar_issues_list').append('<li>+' + more + ' ' + dym.label_more + '</li>');
+    } else {
+      getElem('similar_issues_list').insert({bottom: '<li>+' + more + ' ' + dym.label_more + '</li>'});
+    }
   }
 }
 
@@ -57,35 +88,48 @@ function displayItem(item) {
   var project_name = sanitize(item.project_name);
 
   var item_html = '<li><a href="' + issue_url + '">' 
-    + tracker_name 
-    + ' ' + item_id
-    + ' &ndash; ' 
-    + item_subject
-    + '</a> ('
-    + item_status
-    + ' ' 
-    + dym.label_in
-    + ' ' + project_name
-    + ')</li>';
+                  + tracker_name 
+                  + ' ' + item_id
+                  + ' &ndash; ' 
+                  + item_subject
+                  + '</a> ('
+                  + item_status
+                  + ' ' 
+                  + dym.label_in
+                  + ' ' + project_name
+                  + ')</li>';
 
-  return item_html;
+return item_html;
 }
 
 function sanitize(value) {
-  
+
   var html_safe = value.replace(/[<]+/g, '&lt;')
-                      .replace(/[>]+/g, '&gt;')
-                      .replace(/["]+/g, '&quot;')
-                      .replace(/[']+/g, '&#039;');
+                       .replace(/[>]+/g, '&gt;')
+                       .replace(/["]+/g, '&quot;')
+                       .replace(/[']+/g, '&#039;');
   return html_safe;
 }
 
 function emptySimilarIssuesBlock() {
-  
-  $('similar_issues_list').innerHTML = '';
 
-  if ($('similar_issues').visible()) {
-    $('similar_issues').hide();
+  setText(getElem('similar_issues_list'), '');
+  getElem('similar_issues').hide();
+
+}
+
+function getElem(element_id) {
+  var the_id = element_id;
+  if (window.jQuery) {
+    the_id = '#' + the_id;
   }
+  return $(the_id);
+}
 
+function setText(elem, value) {
+  if (elem.text) {
+    elem.text(value);
+  } else {
+    elem.innerHTML = value;
+  }
 }
