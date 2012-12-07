968821e93e664ef04e1662da16afb1842565e285
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-12-07 19:39:56 +0100

    Removed JSON format from route definition
    Specifying `:format => 'json'` in the route definition made the plugin
    url look like `/searchissues.json` which did not support proper user
    authentication when *Authentication Required* is enabled in the general
    Redmine settings.
    
    This change fixes #28

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 if Rails::VERSION::MAJOR < 3
   ActionController::Routing::Routes.draw do |map|
-    map.plugin_route 'searchissues', :controller => "search_issues", :action => 'index', :format => 'json'
+    map.plugin_route 'searchissues', :controller => "search_issues", :action => 'index'
   end
 else
-  match 'searchissues', :controller => "search_issues", :action => 'index', :format => 'json'
+  match 'searchissues', :controller => "search_issues", :action => 'index'
 end
