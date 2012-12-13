8fc9cc8e9f2d21e87bbb33f8ede0d49fc9da58cc
Alessandro Bahgat <ale.bahgat@gmail.com>
2012-12-13 21:59:59 +0100

    Updated version number to 1.2.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_didyoumean do
   name 'Did You Mean?'
   author 'Alessandro Bahgat and Mattia Tommasone'
   description 'A plugin to search for duplicate issues before opening them.'
-  version '1.1.0'
+  version '1.2.0'
   url 'http://www.github.com/abahgat/redmine_didyoumean'
   author_url 'http://abahgat.com/'
 
