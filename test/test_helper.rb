deded2099d0ffec40ffe2acb0de650d6804c86b0
Vladimir <nettsundere@gmail.com>
2013-01-14 13:07:06 +0400

    Redmine 2 tests fixed.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,2 @@
 # Load the normal Rails helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
